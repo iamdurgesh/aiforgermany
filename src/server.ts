dea9c11d45d34146ad082d2aa03f85484dab8d24
iamdurgesh <durgesh.april25@gmail.com>
2026-04-08 22:39:21 +0200

    stricter CSP script

--- src/server.ts
+++ src/server.ts
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
       "img-src 'self' data: https:",
       "font-src 'self' data:",
       "style-src 'self' 'unsafe-inline'",
-      "script-src 'self'",
+      "script-src 'self' 'unsafe-inline'",
       "connect-src 'self' https:",
       "object-src 'none'",
       "upgrade-insecure-requests",
